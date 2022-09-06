0878516380df13f110bc5ea9f900860ddfc8de73
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2022-09-06 13:36:05 +0200

    Add: IRSA for ESO
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

---
+++ variables.tf
@@ -0,0 +1,37 @@
+# Copyright (C) Nicolas Lamirault <nicolas.lamirault@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#############################################################################
+# External Secrets Operator
+
+variable "cluster_name" {
+  type        = string
+  description = "Name of the EKS cluster"
+}
+
+variable "eso_role_name" {
+  description = "The name of the IAM role"
+  type        = string
+  default     = "eso-controller"
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+}
+
+variable "eso_namespace" {
+  description = "The K8s namespace for ESO resources"
+  type        = string
+}
